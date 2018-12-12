d4bb37c61d3c0a4b3565757623d116e1b3c8003d
earthcomcom <j_sirapob@kkumail.com>
2018-12-13 02:52:16 +0700

    test web

---
+++ script/script.js
@@ -0,0 +1,24 @@
+// $(document).ready(function() {
+//   $("img").click(function() {
+//     $(movieName).html(this.id);
+//   });
+// });
+
+// $(document).ready(function() {
+//   $("td").click(function() {
+//     if (this.id !== "") {
+//       var seat = this.id;
+//       console.log(seat);
+//       document.getElementById(seat).style.backgroundColor = "green";
+      
+//     }
+//   });
+// });
+
+
+
+
+
+
+
+
